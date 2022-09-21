71cddc88f60e3fa7fa8d41b0883e4d510d823181
MYOYOSHINYA <111564464+MYOYOSHINYA@users.noreply.github.com>
2022-09-21 09:27:33 +0900

    Create 1.04.cpp

---
+++ 1.04.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+ 
+  // 以下のコメント/* */を消して追記する
+  std::cout << seconds << std::endl;
+  std::cout << seconds*2 <<std::endl;
+  std::cout << seconds*5 << std::endl;
+  std::cout << seconds*10 << std::endl;
+}
